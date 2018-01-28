25a66ec2a402ec044924ec699e5f74c8486a1f64
DaphneGroot <daphnegroot1996@hotmail.com>
2018-01-28 02:08:59 +0100

    updated gender with LSTM - beginning

--- svm_gender.py
+++ svm_gender.py
@@ -25,10 +25,14 @@ from sklearn.feature_extraction import DictVectorizer
 import numpy as np
 
 from keras.models import Sequential
+from keras.layers.core import Dense, Activation
 from keras.layers import Dense, Dropout, Embedding, Flatten, Conv1D
 from keras.layers import LSTM
 from keras.preprocessing import sequence
 from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelBinarizer
 
 
 def main():
@@ -67,33 +71,84 @@ def main():
         #create seperate lists for tweets and the genders
         train_tweets, train_genders = createLists(trainDocuments)
         test_tweets, test_genders = createLists(testDocuments)
+
+        tweets = train_tweets+test_tweets
+        genders = train_genders+test_genders
+
         tokenizer = Tokenizer()
-        tokenizer.fit_on_texts(train_tweets+test_tweets)
-        X = tokenizer.texts_to_sequences(train_tweets+test_tweets)
-        X_train = X[:int(len(X)*0.8)]
-        X_test = X[int(len(X)*0.8):]
-        X_train = sequence.pad_sequences(X_train, maxlen=120)
-        X_testn = sequence.pad_sequences(X_test, maxlen=120)
+        tokenizer.fit_on_texts(tweets)
+        X = tokenizer.texts_to_sequences(tweets)
+
+        word_index = tokenizer.word_index
+        print('Found %s unique tokens.' % len(word_index))
+
+        X = pad_sequences(X)
+
+        genders = np.asarray(genders)
+        # genders = to_categorical(list(map(str,genders)))
+
+        encoder = LabelBinarizer()
+        genders = encoder.fit_transform(genders) # Use encoder.classes_ to find mapping of one-hot indices to string labels
+        genders = np.where(genders == 1, [0,1], [1,0])
+
+        # print(X[:5])
+        # print(genders[:5])
+        print('Shape of data tensor:', X.shape)
+        print('Shape of label tensor:', genders.shape)
+
+
+        X_train = X[:len(train_tweets)]
+        Y_train = genders[:len(train_genders)]
+
+        X_test = X[len(train_tweets):]
+        Y_test = genders[len(train_tweets):]
+
+
+        X_train = sequence.pad_sequences(X_train, X.shape[1])
+        X_test = sequence.pad_sequences(X_test, X.shape[1])
+
+        Y_train = sequence.pad_sequences(Y_train, genders.shape[1])
+        Y_test = sequence.pad_sequences(Y_test, genders.shape[1])
+
+
         total_words = 5000
         max_length = 120
         embedding_vector_length = 32
+        batch_size = 32
+        epochs = 5
 
-        classifier = classify_lstm(X_train, train_genders, total_words, max_length, embedding_vector_length, 500, 0.2, "relu")
-
+        # classifier = classify_lstm(X, genders, X_train, Y_train, total_words, max_length, embedding_vector_length, 500, 0.2, "relu")
+        classifier = classify_lstm(X, genders, X_train, Y_train, batch_size = batch_size, epochs = epochs)
 
         predicted_genders = classifier.predict(X_test)
+
+        print(predicted_genders[:10])
+
+        n = 0
+        for i in predicted_genders[:10]:
+            print(max(i))
+            if max(i) >= 0.95:
+                n += 1
+
+        print(n)
+
+        predicted_genders = np.argmax(predicted_genders, axis = 1)
+        Y_test = np.argmax(Y_test, axis = 1)
+
+        print(predicted_genders[:10])
+        # print(Y_test[:10])
         
-        accuracy = accuracy_score(test_genders, predicted_genders)
+        accuracy = accuracy_score(Y_test, predicted_genders)
         print("\nAccuracy: ", accuracy)
 
         
-        metricsPerClass = classification_report(test_genders, predicted_genders)
+        metricsPerClass = classification_report(Y_test, predicted_genders)
         print("\nMetrics per class:\n",metricsPerClass)
 
         total_time = time.time() - t0
         print("total time Gender: ", total_time)
         
-        # confusionMatrix = sklearn.metrics.confusion_matrix(test_genders, predicted_genders)
+        # confusionMatrix = sklearn.metrics.confusion_matrix(Y_test, predicted_genders)
     
     
 def identity(x):
@@ -114,18 +169,39 @@ def customLemmatizer(arg):
     st = LancasterStemmer()
     return st.stem(wnl.lemmatize(arg))
 
-def classify_lstm(train_X, train_Y, total_words, max_len, emb_vec_len, nodes, dropout, lstm_activation):
-    train_X = train_X.reshape(len(train_X), 1, len(train_X[0]))
-    #dev_X = dev_X.reshape((len(dev_X), 1, len(dev_X[0])))
-    model = Sequential()
-    #model.add(Embedding(total_words, emb_vec_len, input_length=max_len))
-    model.add(LSTM(nodes, input_shape=(len(train_X[0]), len(train_X[0][0])), dropout=dropout, recurrent_dropout=dropout, return_sequences=True, activation=lstm_activation))  
-    model.add(LSTM(nodes, dropout=dropout, recurrent_dropout=dropout, activation=lstm_activation)) 
-    model.add(Dense(int(nodes/2), activation="relu"))
-    model.add(Dense(int(nodes/4), activation="relu"))
-    model.add(Dense(1, activation='sigmoid'))                       
-    model.compile(loss='mse', optimizer="adam", metrics=['mse']) 
+# def classify_lstm(X, Y, train_X, train_Y, total_words, max_len, emb_vec_len, nodes, dropout, lstm_activation):
+    # train_X = train_X.reshape(len(train_X), 1, len(train_X[0]))
+    # #dev_X = dev_X.reshape((len(dev_X), 1, len(dev_X[0])))
+    # model = Sequential()
+    # #model.add(Embedding(total_words, emb_vec_len, input_length=max_len))
+    # model.add(LSTM(nodes, input_shape=(len(train_X[0]), len(train_X[0][0])), dropout=dropout, recurrent_dropout=dropout, return_sequences=True, activation=lstm_activation))  
+    # model.add(LSTM(nodes, dropout=dropout, recurrent_dropout=dropout, activation=lstm_activation)) 
+    # model.add(Dense(int(nodes/2), activation="relu"))
+    # model.add(Dense(int(nodes/4), activation="relu"))
+    # model.add(Dense(1, activation='sigmoid'))                       
+    # model.compile(loss='mse', optimizer="adam", metrics=['mse']) 
     
+
+
+    # model = Sequential()
+    # model.add(Dense(32, input_shape=(X.shape[1],)))
+    # model.add(Dense(Y.shape[1], activation='relu'))
+    # model.compile(optimizer='adam',
+    #               loss='binary_crossentropy',
+    #               metrics=['accuracy'])
+
+def classify_lstm(X, Y, train_X, train_Y, batch_size, epochs):
+    model = Sequential()
+    model.add(Dense(input_dim=X.shape[1], units=Y.shape[1]))
+    model.add(Activation("linear"))
+    model.compile(optimizer='adam',
+                  loss='mean_squared_error',
+                  metrics=['accuracy'])
+
+    model.fit(train_X, train_Y,
+                        batch_size=batch_size,
+                        epochs=epochs)
+
     return model
 
 
